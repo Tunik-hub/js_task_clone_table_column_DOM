c656c181843c655559417a0c80a7a517e5a4a2c4
Vadym Bakuma <bakuma.2001@gmail.com>
2024-06-14 18:49:34 +0200

    make build

---
+++ index.81ca1806.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,l=0;l<e.length;l++){var r=e[l].cells[1].cloneNode(!0);e[l].insertBefore(r,e[l].cells[e[l].cells.length-1])}
+//# sourceMappingURL=index.81ca1806.js.map
